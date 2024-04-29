5fb5e36392f512400b3aa45c51e042e1f75f7aa7
Padam7890 <padamthapamoho@gmail.com>
2024-04-29 08:35:16 +0545

    new

--- routes/adRoute.js
+++ routes/adRoute.js
@@ -8,12 +8,14 @@ const editad = require("../controller/advertisment/onead");
 const deletead = require("../controller/advertisment/deletad");
 const updatead = require("../controller/advertisment/updatead");
 const uploadToCloudinary = require("../middleware/cloudsave");
+const checkAuth = require("../middleware/auth");
+const checkPermission = require("../middleware/restrict");
 
 router.get('/',   getadvertisment);
 router.post('/',  upload.single('image'), uploadToCloudinary,  createad);
 router.put('/:id',  upload.single('image'), uploadToCloudinary,  updatead);
 router.get('/:id',  editad );
-router.delete('/:id',  deletead )
+router.delete('/:id', checkAuth, checkPermission(['delete']) ,  deletead )
 
 
 module.exports = router;
