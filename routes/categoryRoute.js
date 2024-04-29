488fa4ccc24aaf43ad24ee3318f3926167ef945b
Padam7890 <padamthapamoho@gmail.com>
2024-04-29 16:18:43 +0545

    new added

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -9,12 +9,13 @@ const deleteCategory = require("../controller/category/delete");
 const checkAuth = require("../middleware/auth");
 const restrict = require("../middleware/restrict");
 const uploadToCloudinary = require("../middleware/cloudsave");
+const checkPermission = require("../middleware/restrict");
 
 router.get("/", getAllCategory);
 router.get("/:id", getCategoryByID);
 router.post("/", upload.single("image") , uploadToCloudinary, createCategory);
 router.put("/:id", upload.single("image"),uploadToCloudinary, updateCategory);
-router.delete("/:id", checkAuth, deleteCategory);
+router.delete("/:id", checkAuth, checkPermission(["delete"]) , deleteCategory);
 
 module.exports = router;
 
