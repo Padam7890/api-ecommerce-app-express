ddde48697e41abb6c05e8f5cb2fcce976727b18e
Padam7890 <padamthapamoho@gmail.com>
2024-04-29 11:27:12 +0545

    bug fixed

--- controller/category/delete.js
+++ controller/category/delete.js
@@ -9,56 +9,55 @@ async function deleteCategory(request, response) {
   const { id } = request.params;
   console.log(id);
   try {
-    await prisma.$transaction(async (prisma) => {
-      // Check if the category exists
-      const categoryToDelete = await category.findUnique({
-        where: { id: parseInt(id) },
+    // Check if the category exists
+    const categoryToDelete = await category.findUnique({
+      where: { id: parseInt(id) },
+    });
+
+    console.log(categoryToDelete);
+
+    if (!categoryToDelete) {
+      return response.status(404).json({
+        message: "Category not found",
+        category: null,
       });
-      
+    }
 
-      if (!categoryToDelete) {
-        return response.status(404).json({
-          message: "Category not found",
-          category: null,
-        });
-      }
+    // Check if the category has associated products
+    const associatedProducts = await prisma.product.findMany({
+      where: {
+        category_id: parseInt(id),
+      },
+    });
 
-      // Check if the category has associated products
-      const associatedProducts = await prisma.product.findMany({
-        where: {
-          category_id: parseInt(id),
-        },
+    // Check if the category has associated subcategories
+    const associatedSubCategories = await prisma.subcategory.findMany({
+      where: {
+        category_id: parseInt(id),
+      },
+    });
+
+    if (associatedProducts.length > 0) {
+      return response.status(400).json({
+        message: "Cannot delete category. It has associated products",
+        category: null,
       });
+    }
 
-      // Check if the category has associated subcategories
-      const associatedSubCategories = await prisma.subcategory.findMany({
-        where: {
-          category_id: parseInt(id),
-        },
+    if (associatedSubCategories.length > 0) {
+      return response.status(400).json({
+        message: "Cannot delete category. It has associated subcategories",
+        category: null,
       });
+    }
 
-      if (associatedProducts.length > 0) {
-        return response.status(400).json({
-          message: "Cannot delete category. It has associated products",
-          category: null,
-        });
-      }
+    // Delete the category from the database
+    await prisma.category.delete({ where: { id: parseInt(id) } });
 
-      if (associatedSubCategories.length > 0) {
-        return response.status(400).json({
-          message: "Cannot delete category. It has associated subcategories",
-          category: null,
-        });
-      }
-
-      // Delete the category from the database
-      await prisma.category.delete({ where: { id: parseInt(id) } });
-
-      // If everything is successful, send a success response
-      return response.status(200).json({
-        message: "Category deleted successfully",
-        category: categoryToDelete,
-      });
+    // If everything is successful, send a success response
+    return response.status(200).json({
+      message: "Category deleted successfully",
+      category: categoryToDelete,
     });
   } catch (error) {
     // Handle any errors that occur during the transaction
