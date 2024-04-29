179cdbd5cfa3500799c9ba99f371072b4554c758
Padam7890 <padamthapamoho@gmail.com>
2024-04-29 08:12:11 +0545

    new bug fixed

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "djsdxexto",
-  api_key: "498133595171588",
-  api_secret: "qcWMIx583KHPg1tbiGKAH5KA3ck",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
